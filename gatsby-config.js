0dae723f04edf50341e66b32526faa55b5e2a217
Yudai Yaguchi <yudaiyaguchi@Yudais-MacBook-Pro-2.local>
2020-01-08 15:39:35 +0900

    created all the tours

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: 'BackRoads',
+    description:
+      'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+    author: '@yudai',
+  },
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-playground`,
+  ],
+}
